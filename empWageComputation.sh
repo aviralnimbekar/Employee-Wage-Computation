2fde353bdf0250a6d4fb18a74cad2903f3b1345c
amitnimbekar <amit.nimbekar@actifio.com>
2021-07-24 22:38:41 +0530

    Employee daily wage/salary

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,10 +4,15 @@ echo "Welcome to Employee Wage Computation Program"
 
 isPresent=1
 randomcheck=$((RANDOM % 2))
+wage_Per_Hrs=20
 
 if [ $isPresent == $randomcheck ]
 then
 	echo "employee is present"
+	working_Hrs=8
 else 
 	echo "employee is absent"
 fi
+
+salary=$((wage_Per_Hrs * working_Hrs))
+echo $salary
