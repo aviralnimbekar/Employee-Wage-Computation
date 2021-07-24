ab5cb057a88be3baf58f690731d90d9f5fdc8e4c
amitnimbekar <amit.nimbekar@actifio.com>
2021-07-24 21:13:18 +0530

    Employee Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program"
+
+isPresent=1
+randomcheck=$((RANDOM % 2))
+
+if [ $isPresent == $randomcheck ]
+then
+	echo "employee is present"
+else 
+	echo "employee is absent"
+fi
