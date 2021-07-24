11dc1ed059fa81894951ce290ba8b5f6597e2885
amitnimbekar <amit.nimbekar@actifio.com>
2021-07-24 23:05:21 +0530

    Solved same problem using Switch Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,23 +2,17 @@
 
 echo "Welcome to Employee Wage Computation Program"
 
-isFullTimePresent=1
-isPartTimePresent=2
 randomcheck=$((RANDOM % 3))
 wage_Per_Hrs=20
 
-if [ $isFullTimePresent == $randomcheck ]
-then
-	echo "employee is full time present"
-	working_Hrs=8
-elif [ $isPartTimePresent == $randomcheck ]
-then
-	echo "employee is part time present"
-	working_Hrs=4
-else 
-	echo "employee is absent"
-	working_Hrs=0
-fi
+case $randomcheck in
+	1) echo "employee is full time present"
+	   working_Hrs=8				;;
+	2) echo "employee is part time present"
+	   working_Hrs=4				;;
+	3) echo "employee is absent"
+	   working_Hrs=0				;;
+esac
 
 salary=$((wage_Per_Hrs * working_Hrs))
 echo $salary
