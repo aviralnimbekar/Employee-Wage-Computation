e7d4ef3d0b26601d5ca6bb23706741efea4f1637
amitnimbekar <amit.nimbekar@actifio.com>
2021-07-24 22:55:10 +0530

    Part time employees

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,16 +2,22 @@
 
 echo "Welcome to Employee Wage Computation Program"
 
-isPresent=1
-randomcheck=$((RANDOM % 2))
+isFullTimePresent=1
+isPartTimePresent=2
+randomcheck=$((RANDOM % 3))
 wage_Per_Hrs=20
 
-if [ $isPresent == $randomcheck ]
+if [ $isFullTimePresent == $randomcheck ]
 then
-	echo "employee is present"
+	echo "employee is full time present"
 	working_Hrs=8
+elif [ $isPartTimePresent == $randomcheck ]
+then
+	echo "employee is part time present"
+	working_Hrs=4
 else 
 	echo "employee is absent"
+	working_Hrs=0
 fi
 
 salary=$((wage_Per_Hrs * working_Hrs))
