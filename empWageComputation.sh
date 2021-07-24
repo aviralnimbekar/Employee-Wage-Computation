f8149a883976c5ee4458f7db2b7c75b07daad123
amitnimbekar <amit.nimbekar@actifio.com>
2021-07-25 03:28:45 +0530

    Wage till 100 Working Hrs

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,14 +6,18 @@ randomcheck=$((RANDOM % 3))
 wage_Per_Hrs=20
 working_Day_Per_month=20
 
-case $randomcheck in
-	1) echo "employee is full time present"
-	   working_Hrs=8				;;
-	2) echo "employee is part time present"
-	   working_Hrs=4				;;
-	3) echo "employee is absent"
-	   working_Hrs=0				;;
-esac
+if [ $randomcheck == 1 ]
+then
+	echo "employee is full time present"
+	working_Hrs=5
+elif [ $randomcheck == 2 ]
+then
+	echo "employee is part time present"
+	working_Hrs=2.5
+else
+	echo "employee is absent"
+	working_Hrs=0
+fi
 
-salary=$((wage_Per_Hrs * working_Day_Per_month * working_Hrs))
+salary=`awk "BEGIN {print $wage_Per_Hrs * $working_Day_Per_month * $working_Hrs}"`
 echo $salary
